adc8e4b9ec4e3b2dc75572f58cf6e5ed97ca81cd
Vinicius Petroli Affonso <126300608+viniciuspetroli@users.noreply.github.com>
2025-01-13 15:16:52 -0300

    changed return

--- app/src/dynamo.py
+++ app/src/dynamo.py
@@ -9,14 +9,21 @@ class DynamoService:
     
     def put_item(self, item):
         self.client.put_item(TableName=self.table,Item=item)
-        return "Registro inserido com sucesso"
+        return {
+            "statusCode": 200,
+            "body":"Registro inserido com sucesso"
+        }
     
     def update_item(self, key, update_expression, att_values):
         self.client.update_item(TableName=self.table, Key=key, UpdateExpression=update_expression, ExpressionAttributeValues=att_values)
-        return "Registro atualizado com sucesso"
+        return {
+            "statusCode": 200,
+            "body":"Registro atualizado com sucesso"
+        }
     
     def delete_item(self, key):
         self.client.delete_item(TableName=self.table, Key=key)
-        return "Registro deletado com sucesso"
-    
-    #Just a comment to push the code
+        return {
+            "statusCode": 200,
+            "body":"Registro deletado com sucesso"
+        }
